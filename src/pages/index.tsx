0580adb3b02fc6d8c2cc6f49dff449b392c1bfb2
firiexp <firizfiriz300@hotmail.co.jp>
2022-12-06 23:39:44 +0900

    make index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, Link as ChakraLink } from '@chakra-ui/react';
+import { Heading, Link as ChakraLink, Text, ListItem, UnorderedList } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import Container from '../components/Container';
 import Main from '../components/Main';
@@ -12,13 +12,31 @@ function Index() {
       <Container>
         <Header />
         <Main>
-          <Heading>Maple Calculator</Heading>
-          <NextLink href='/sf' passHref>
-            <ChakraLink>スターフォースシミュレーター</ChakraLink>
-          </NextLink>
-          <NextLink href='/statcalculator' passHref>
-            <ChakraLink>ステータス計算機</ChakraLink>
-          </NextLink>
+          <Heading as='h1' size='xl'>
+            Maple Calculator
+          </Heading>
+          <Text>メイプルストーリーに関するツールが使えます。開発中です。</Text>
+          <Heading as='h2' size='md'>
+            ツール一覧
+          </Heading>
+          <UnorderedList>
+            <ListItem>
+              <NextLink href='/sf' passHref>
+                <ChakraLink color='teal.400'>スターフォースシミュレーター</ChakraLink>
+              </NextLink>
+              <UnorderedList>
+                <ListItem>スターフォースが体験できるだけ。実用性0のツールです。</ListItem>
+              </UnorderedList>
+            </ListItem>
+            <ListItem>
+              <NextLink href='/statcalculator' passHref>
+                <ChakraLink color='teal.400'>ステータス計算機</ChakraLink>
+              </NextLink>
+              <UnorderedList>
+                <ListItem>ステータスを入力して効率を計算できるやつです。</ListItem>
+              </UnorderedList>
+            </ListItem>
+          </UnorderedList>
         </Main>
       </Container>
     </>
